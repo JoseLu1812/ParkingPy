65e6939f9c389ed16a5fc58d0bb2e09f92da534d
José Luis Hidalgo Navas <jhidalgonavas@gmail.com>
2023-01-17 23:18:13 +0100

    Creacion final de entidades y comienzo del service

---
+++ parkingJL/Entities/tipovehiculo.py
@@ -0,0 +1,7 @@
+from enum import Enum
+class TipoVehiculo(Enum):
+
+    TURISMO = "Turismo"
+    MOTOCICLETA = "Motocicleta"
+    MOVILIDADREDUCIDA = "Movilidad reducidad"
+
---
+++ parkingJL/Services/services.py
@@ -0,0 +1,10 @@
+
+
+from parkingJL.Entities.abonado import Abonado
+from parkingJL.Entities.cliente import Cliente
+from parkingJL.Entities.disponibilidad import Disponibilidad
+from parkingJL.Entities.ocupa import Ocupa
+from parkingJL.Entities.plaza import Plaza
+from parkingJL.Entities.tipoabono import TipoAbono
+from parkingJL.Entities.tipoplaza import TipoPlaza
+from parkingJL.Entities.tipovehiculo import TipoVehiculo
